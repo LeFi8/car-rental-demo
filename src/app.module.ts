d6844d73bbac36d4483d7769222e4ec1b604a51b
lef8 <alan.sipiorski14@gmail.com>
2023-08-18 16:01:20 +0200

    Add postgres db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { CarsModule } from './cars/cars.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
     imports: [
-        GraphQLModule.forRoot({
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
             autoSchemaFile: true,
+        }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'root',
+            password: 'root',
+            database: 'car_rental_db',
+            entities: ['dist/**/*.entity.js'],
+            synchronize: true,
         }),
         CarsModule,
     ],
