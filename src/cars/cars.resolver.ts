f9dbbd8b6ce16708d925cf6af70ff1552a3698e1
lef8 <alan.sipiorski14@gmail.com>
2023-08-18 16:02:15 +0200

    Implement graphql mutation for creating new car

--- src/cars/cars.resolver.ts
+++ src/cars/cars.resolver.ts
@@ -1,4 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CarsService } from './cars.service';
+import { Car } from './car.entity';
+import { CreateCarInput } from './dto/create-car.input';
 
-@Resolver()
-export class CarsResolver {}
+@Resolver(() => Car)
+export class CarsResolver {
+    constructor(private carsService: CarsService) {}
+
+    @Query(() => [Car])
+    cars(): Promise<Car[]> {
+        return this.carsService.findAll();
+    }
+
+    @Mutation(() => Car)
+    createCar(
+        @Args('createCarInput') createCarInput: CreateCarInput,
+    ): Promise<Car> {
+        return this.carsService.createCar(createCarInput);
+    }
+}
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Car } from './car.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateCarInput } from './dto/create-car.input';
 
 @Injectable()
-export class CarsService {}
+export class CarsService {
+    constructor(
+        @InjectRepository(Car) private carsRepository: Repository<Car>,
+    ) {}
+
+    createCar(createCarInput: CreateCarInput): Promise<Car> {
+        const newCar = this.carsRepository.create(createCarInput);
+        return this.carsRepository.save(newCar);
+    }
+
+    async findAll(): Promise<Car[]> {
+        return this.carsRepository.find();
+    }
+}
