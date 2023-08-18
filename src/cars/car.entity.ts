8957086cff87c10dda1343070305cb3f0ea865e0
lef8 <alan.sipiorski14@gmail.com>
2023-08-18 15:59:31 +0200

    Create car entity file

---
+++ src/cars/car.entity.ts
@@ -0,0 +1,20 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class Car {
+    @PrimaryGeneratedColumn({
+        name: 'id_car',
+    })
+    @Field(() => Int)
+    id: number;
+
+    @Column()
+    @Field()
+    brand: string;
+
+    @Column()
+    @Field()
+    year: number;
+}
