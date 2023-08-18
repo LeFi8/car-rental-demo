beee6910ca3b4493c2acb28e16d4ce8f753dfcb0
lef8 <alan.sipiorski14@gmail.com>
2023-08-18 13:44:03 +0200

    Create cars module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        GraphQLModule.forRoot({
+            autoSchemaFile: true,
+        }),
+        CarsModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
